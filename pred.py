04134835d9139d51aa1e2d3ca93f56fad80d3977
Vansh Gurnani <gurnanivansh57@gmail.com>
2025-01-16 01:27:09 +0530

    sciket learn

--- pred.py
+++ pred.py
@@ -1,6 +1,4 @@
 import pickle  # Used to load the saved model and scaler
-from sklearn.preprocessing import StandardScaler  # Import only the required scaler
-from sklearn.ensemble import RandomForestClassifier  # Import the model type used
 
 class DogHealthPredictor:
     
